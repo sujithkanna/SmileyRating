04bfc95a55e22743d604e0d253e1e3b0d42f88c8
Sujith kanna <sujith.niraikulathan@gmail.com>
2016-10-15 01:15:05 +0530

    Add seekbar to test smile

--- app/src/main/java/com/msapps/smilyrating/MainActivity.java
+++ app/src/main/java/com/msapps/smilyrating/MainActivity.java
@@ -2,17 +2,46 @@ package com.msapps.smilyrating;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
+import android.widget.SeekBar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
 
-    private ImageView mImageView;
+    private RatingView mRatingView;
+    private SeekBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRatingView = (RatingView) findViewById(R.id.ratingView);
+        mProgressBar = (SeekBar) findViewById(R.id.progress);
+        mProgressBar.setOnSeekBarChangeListener(this);
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mRatingView.start();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mRatingView.stop();
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+        mRatingView.setFraction(i / 100f);
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
 
     }
 }
--- app/src/main/java/com/msapps/smilyrating/RatingView.java
+++ app/src/main/java/com/msapps/smilyrating/RatingView.java
@@ -1,5 +1,7 @@
 package com.msapps.smilyrating;
 
+import android.animation.FloatEvaluator;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -7,11 +9,12 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.animation.DecelerateInterpolator;
 
 /**
  * Created by sujith on 11/10/16.
  */
-public class RatingView extends View {
+public class RatingView extends View implements ValueAnimator.AnimatorUpdateListener {
 
     private static final String TAG = "RatingView";
 
@@ -19,6 +22,9 @@ public class RatingView extends View {
     private Paint mPointPaint1 = new Paint();
     private Paint mPointPaint2 = new Paint();
     private Path mDrawingPath = new Path();
+
+    private ValueAnimator mValueAnimator = new ValueAnimator();
+    private FloatEvaluator mFloatEvaluator = new FloatEvaluator();
 
     public RatingView(Context context) {
         super(context);
@@ -45,26 +51,47 @@ public class RatingView extends View {
 
         mPointPaint2.setColor(Color.GREEN);
         mPointPaint2.setStyle(Paint.Style.FILL);
-        refreshPath();
+
+        mValueAnimator.setDuration(500);
+        mValueAnimator.setIntValues(0, 100);
+        mValueAnimator.addUpdateListener(this);
+        mValueAnimator.setRepeatMode(ValueAnimator.INFINITE);
+        mValueAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        mValueAnimator.setInterpolator(new DecelerateInterpolator());
     }
 
-    private void refreshPath() {
+    private void refreshPath(float fraction) {
         mDrawingPath.reset();
 
         mDrawingPath.moveTo(100, 500);
         // top curve
         mDrawingPath.cubicTo(150, 500, 350, 500, 400, 500);
         //Right curve
-        mDrawingPath.cubicTo(450, 500, 450, 550, 400, 600);
+        mDrawingPath.cubicTo(450, 500, 450, 550, 400, mFloatEvaluator.evaluate(fraction, 600, 550));
 
         // bottom curve
-        mDrawingPath.cubicTo(325, 675, 175, 675, 100, 600);
+        mDrawingPath.cubicTo(325, mFloatEvaluator.evaluate(fraction, 675, 550),
+                mFloatEvaluator.evaluate(fraction, 175, 150),
+                mFloatEvaluator.evaluate(fraction, 675, 550), 100,
+                mFloatEvaluator.evaluate(fraction, 600, 550));
 
         // Left curve
         mDrawingPath.cubicTo(50, 550, 50, 500, 100, 500);
 
         mDrawingPath.close();
     }
+
+    /*mDrawingPath.moveTo(100, 500);
+    // top curve
+    mDrawingPath.cubicTo(150, 500, 350, 500, 400, 500);
+    //Right curve
+    mDrawingPath.cubicTo(450, 500, 450, 550, 400, 550);
+
+    // bottom curve
+    mDrawingPath.cubicTo(350, 550, 150, 550, 100, 550);
+
+    // Left curve
+    mDrawingPath.cubicTo(50, 550, 50, 500, 100, 500);*/
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
@@ -79,4 +106,23 @@ public class RatingView extends View {
         canvas.drawPath(mDrawingPath, mPathPaint);
     }
 
+    @Override
+    public void onAnimationUpdate(ValueAnimator valueAnimator) {
+        float fraction = valueAnimator.getAnimatedFraction();
+        refreshPath(fraction);
+        invalidate();
+    }
+
+    public void start() {
+//        mValueAnimator.start();
+    }
+
+    public void stop() {
+//        mValueAnimator.end();
+    }
+
+    public void setFraction(float fraction) {
+        refreshPath(fraction);
+        invalidate();
+    }
 }
