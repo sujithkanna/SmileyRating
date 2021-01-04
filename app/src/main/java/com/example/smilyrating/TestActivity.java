d73cef2b1c4dd24f3de52301372c0285cc19c3bf
jaws0227 <jaws0910@gmail.com>
2021-01-04 17:26:13 +0900

    Move SmileyView from App module to SmileRating module   I want SmileyView because my project showed SmileyView   I think this is not just my project.

--- app/src/main/java/com/example/smilyrating/TestActivity.java
+++ app/src/main/java/com/example/smilyrating/TestActivity.java
@@ -4,11 +4,16 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.hsalf.smileyrating.SmileyView;
+
 public class TestActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+
+        SmileyView smileyView = findViewById(R.id.smiley_view);
+        smileyView.setSmiley(3);
     }
 }
